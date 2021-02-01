f3e46e88939cb0adb61395336ce666e352a864f7
Yeongil Yoon <askme143@kaist.ac.kr>
2021-02-01 16:44:04 +0900

    Fix keyword inclusion text

--- app/src/main/java/com/yeongil/digitalwellbeing/viewModel/item/TriggerActionItem.kt
+++ app/src/main/java/com/yeongil/digitalwellbeing/viewModel/item/TriggerActionItem.kt
@@ -44,7 +44,7 @@ class TriggerActionItem(val title: String, val description: String) {
 
     constructor(locationTrigger: LocationTrigger) : this(
         LOCATION_TRIGGER_TITLE,
-        "${locationTrigger.locationName} (${locationTrigger.range}m)"
+        "${locationTrigger.locationName} ${locationTrigger.range}m 내에 있을 때"
     )
 
     constructor(timeTrigger: TimeTrigger) : this(
