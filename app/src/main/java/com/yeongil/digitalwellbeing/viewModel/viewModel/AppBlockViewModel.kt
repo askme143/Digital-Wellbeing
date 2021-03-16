56ca2a627907846e901526c0bd0f18b0c1670070
Yeongil Yoon <askme143@kaist.ac.kr>
2021-03-16 21:33:19 +0900

    Implement temp use countdown

--- app/src/main/java/com/yeongil/digitalwellbeing/viewModel/viewModel/AppBlockViewModel.kt
+++ app/src/main/java/com/yeongil/digitalwellbeing/viewModel/viewModel/AppBlockViewModel.kt
@@ -3,6 +3,11 @@ package com.yeongil.digitalwellbeing.viewModel.viewModel
 import android.util.Log
 import androidx.lifecycle.*
 import com.yeongil.digitalwellbeing.repository.PackageManagerRepository
+import com.yeongil.digitalwellbeing.utils.combineWith
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AppBlockViewModel(
     private val pmRepo: PackageManagerRepository
@@ -15,8 +20,31 @@ class AppBlockViewModel(
 
     val isClose = MutableLiveData<Boolean>()
 
+    private val _countDown = MutableLiveData(10)
+    val tempUseBtnText = _countDown.combineWith(isClose) { int, bool -> Pair(int, bool) }
+        .map { (sec, bool) ->
+            val countDown = sec ?: 10
+            val isClose = bool ?: true
+
+            val tempUseText = if (isClose) "임시 사용" else "이번 사용만 경고 알림을 출력하지 않습니다."
+            val countDownText = if (countDown == 0) "" else "\n(${countDown})"
+
+            tempUseText + countDownText
+        }
+    val tempUseEnabled = _countDown.map { it == 0 }
+
     fun putPackageName(packageName: String) {
         this.packageName.value = packageName
+        _countDown.value = 10
+
+        viewModelScope.launch {
+            for (i in 1..10) {
+                delay(1 * 1000)
+                withContext(Dispatchers.Main) {
+                    _countDown.value = (_countDown.value ?: 10) - 1
+                }
+            }
+        }
     }
 
     fun putBlockingAppKey(blockingAppKey: String) {
